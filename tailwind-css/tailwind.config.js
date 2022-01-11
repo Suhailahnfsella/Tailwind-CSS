d7ad27b3e0a5e23488b20af66e6688208b88e9db
Suhailahnfsella <68285716+Suhailahnfsella@users.noreply.github.com>
2022-01-12 00:56:55 +0700

    Tugas tailwindcss Smst6 - 10-11 Januari 2022
    Video playlist Tailwind-CSS

---
+++ tailwind-css/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    colors:{
+      'ungu':'#5267DF',
+      'merah':'#FA5959',
+      'biru':'#242A45',
+      'abu-abu':'#9194A2',
+      'putih':'#f7f7f7',
+      'hitam':'#000000',
+    },
+
+    fontFamily:{
+      Poppins:["Poppins, sans-serif"]
+    },
+
+    container:{
+      center:true,
+      padding:"1rem",
+      screens:{
+        lg:"1124px",
+        xl:"1124px",
+        "2xl":"1124px",
+      },
+    },
+    
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
